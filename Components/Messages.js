7584824b41c67929d81b3e7fc8a0305b9b0754fd
Priyansh Gupta <87969057+PriansGupta@users.noreply.github.com>
2024-03-04 02:13:35 +0530

    Individual message

--- Components/Messages.js
+++ Components/Messages.js
@@ -1,8 +1,10 @@
 import Lottie from "react-lottie";
 import NoMessages from "@/Assets/NoMessages.json";
+import { useRouter } from "next/navigation";
 
 function Messages({ messages }) {
-//   console.log(messages);
+  // console.log(messages);
+  const router = useRouter();
   if (messages.length === 0)
     return (
       <div className="text-center mt-6">
@@ -19,25 +21,26 @@ function Messages({ messages }) {
     );
   else
     return (
-      <div>
-        <ul>
-          {messages
-            .slice()
-            .reverse()
-            .map(({ from, timestamp, message }, index) => (
-              <li
-                key={index}
-                className="border-b mb-1 bg-white w-[90%] mx-auto p-4 shadow-md cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out"
-              >
-                <div className="flex justify-between">
-                  <h3 className="text-lg font-semibold">From : {from}</h3>
-                  <h3 className="text-md font-bold">{timestamp}</h3>
-                </div>
-                <p>Message : {message}</p>
-              </li>
-            ))}
-        </ul>
-      </div>
+      <ul>
+        {messages
+          .slice()
+          .reverse()
+          .map(({ from, timestamp, message, _id }, index) => (
+            <li
+              onClick={() => {
+                router.push(`/account/alerts/${_id}`);
+              }}
+              key={index}
+              className="border-b mb-1 bg-white w-[90%] mx-auto p-4 shadow-md cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out"
+            >
+              <div className="flex justify-between">
+                <h3 className="text-lg font-semibold">From : {from}</h3>
+                <h3 className="text-md font-bold">{timestamp}</h3>
+              </div>
+              <p>Message : {message}</p>
+            </li>
+          ))}
+      </ul>
     );
 }
 
---
+++ app/account/alerts/[id]/page.js
@@ -0,0 +1,57 @@
+"use client";
+import Account from "../../page";
+import { useState, useEffect } from "react";
+import SimpleBackdrop from "@/Components/Backdrop";
+import { useRouter } from "next/navigation";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+
+function SingleMessage() {
+  const router = useRouter();
+  const [isLoading, setLoading] = useState(true);
+  const [messageData, setMessageData] = useState({});
+  const [messages, setMessage] = useState(
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage?.getItem("messages"))
+      : {}
+  );
+
+  useEffect(() => {
+    const id = window.location.pathname.split("/").pop();
+    setMessageData(messages.find((message) => message._id === id));
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
+
+  return (
+    <Account>
+      <SimpleBackdrop open={isLoading}></SimpleBackdrop>
+      <div className="mx-auto w-[90%] mt-8 bg-white rounded-lg overflow-hidden shadow-lg">
+        <div onClick={() => router.back()} className="px-6 pt-4 cursor-pointer">
+          <ArrowBackIcon></ArrowBackIcon>
+        </div>
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Message Details</div>
+          <p className="text-gray-700 text-base">
+            <span className="font-bold text-gray-800">From:</span>{" "}
+            {messageData.from}
+          </p>
+          <p className="text-gray-700 text-base mt-2">
+            <span className="font-bold text-gray-800">Message:</span>{" "}
+            {messageData.message}
+          </p>
+          <p className="text-gray-700 text-base mt-2">
+            <span className="font-bold text-gray-800">Timestamp:</span>{" "}
+            {messageData.timestamp}
+          </p>
+          <p className="text-gray-700 text-base mt-2">
+            <span className="font-bold text-gray-800">Message ID:</span>{" "}
+            {messageData._id}
+          </p>
+        </div>
+      </div>
+    </Account>
+  );
+}
+
+export default SingleMessage;
