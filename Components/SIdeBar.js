84e7a62bd16d5e2c021cb448608f34c0eda76c5f
Priyansh Gupta <87969057+PriansGupta@users.noreply.github.com>
2024-03-07 19:05:19 +0530

    Fixes

--- Components/SIdeBar.js
+++ Components/SIdeBar.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useEffect,useContext } from "react";
+import { useEffect, useContext } from "react";
 import { useRouter } from "next/navigation";
 import SimpleBackdrop from "./Backdrop";
 import AddAlertIcon from "@mui/icons-material/AddAlert";
@@ -84,11 +84,14 @@ export default function SIdeBar() {
           onMouseEnter={() => setHover(true)}
           onMouseLeave={() => setHover(false)}
           onClick={() => router.push("/account/cart/")}
-          className={`w-[60%] transition-transform cursor-pointer mt-3 align-middle items-center font-bold tracking-wide text-md text-green-500 mx-auto flex justify-evenly p-2 bg-green-100 rounded-2xl ${
+          className={`w-[60%] custom-card transition-transform cursor-pointer mt-3 align-middle items-center font-bold tracking-wide text-md text-green-500 mx-auto flex justify-evenly p-2 bg-green-100 rounded-2xl ${
             hover ? "scale-105 bg-green-200" : ""
           }`}
         >
-          <Badge badgeContent={Object.values(cartItems).length} color="secondary">
+          <Badge
+            badgeContent={Object.values(cartItems).length}
+            color="secondary"
+          >
             <ShoppingCartIcon fontSize="large"></ShoppingCartIcon>
           </Badge>
           <p>Go to Cart</p>
