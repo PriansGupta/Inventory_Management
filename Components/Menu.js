e7eccc1bc5f5cf0adc0d1646df7a564d0d729d6d
Priyansh Gupta <87969057+PriansGupta@users.noreply.github.com>
2024-03-07 18:48:08 +0530

    Fixes

--- Components/Menu.js
+++ Components/Menu.js
@@ -2,14 +2,20 @@ import { useState } from "react";
 
 function Menu(props) {
   const [hover, setHover] = useState(false);
+  let hoverClass = "";
+
+  if (hover)
+    hoverClass =
+      "mt-2 cursor-pointer transition-all align-middle font-medium items-center tracking-wide text-md mx-auto flex justify-start p-4 rounded-xl bg-green-600 text-white";
+  else
+    hoverClass =
+      "mt-2 cursor-pointer transition-all align-middle font-medium items-center tracking-wide text-md mx-auto flex justify-start p-4 rounded-xl text-gray-500";
   return (
     <div
       onClick={() => props.action()}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
-      className={`mt-2 cursor-pointer transition-all align-middle font-medium items-center tracking-wide text-md mx-auto flex justify-start p-4 rounded-xl ${
-        hover ? "bg-green-600 text-white" : "text-gray-500"
-      }`}
+      className={hoverClass}
     >
       {props.icon}
       <p className="ml-4 text-lg">{props.option}</p>
